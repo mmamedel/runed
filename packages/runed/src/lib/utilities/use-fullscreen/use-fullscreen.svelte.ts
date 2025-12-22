191211227a3556d79c791ba517630b9b3d0cc70b
Mateus Lage <mmamedel@gmail.com>
2025-12-22 19:56:55 -0300

    feat(useFullscreen): align implementation to VueUse

--- packages/runed/src/lib/utilities/use-fullscreen/use-fullscreen.svelte.ts
+++ packages/runed/src/lib/utilities/use-fullscreen/use-fullscreen.svelte.ts
@@ -1,36 +1,170 @@
+import { type ConfigurableDocument, defaultDocument } from "../../internal/configurable-globals.js";
+import type { MaybeElementGetter } from "../../internal/types.js";
+import { extract } from "../extract/extract.svelte.js";
+import { useEventListener } from "../use-event-listener/index.js";
+
+export interface UseFullscreenOptions extends ConfigurableDocument {
+	/**
+	 * Automatically exit fullscreen when component is unmounted
+	 *
+	 * @default false
+	 */
+	autoExit?: boolean;
+}
+
 export interface UseFullscreenReturn {
+	readonly isSupported: boolean;
 	readonly isFullscreen: boolean;
 	enter: () => Promise<void>;
 	exit: () => Promise<void>;
 	toggle: () => Promise<void>;
 }
 
-export function useFullscreen(el: () => HTMLElement | undefined): UseFullscreenReturn {
+const eventHandlers = [
+	"fullscreenchange",
+	"webkitfullscreenchange",
+	"webkitendfullscreen",
+	"mozfullscreenchange",
+	"MSFullscreenChange",
+] as any as "fullscreenchange"[];
+
+export function useFullscreen<T extends Element = HTMLElement>(
+	target?: MaybeElementGetter<T>,
+	options: UseFullscreenOptions = {}
+): UseFullscreenReturn {
+	const { document = defaultDocument, autoExit = false } = options;
+
+	let targetRef = $derived(extract(target) ?? document?.documentElement) as T | null | undefined;
 	let isFullscreen = $state(false);
+
+	const requestMethod = $derived.by<"requestFullscreen" | undefined>(() => {
+		return [
+			"requestFullscreen",
+			"webkitRequestFullscreen",
+			"webkitEnterFullscreen",
+			"webkitEnterFullScreen",
+			"webkitRequestFullScreen",
+			"mozRequestFullScreen",
+			"msRequestFullscreen",
+		].find((m) => (document && m in document) || (targetRef && m in targetRef)) as
+			| "requestFullscreen"
+			| undefined;
+	});
+
+	const exitMethod = $derived.by<"exitFullscreen" | undefined>(() => {
+		return [
+			"exitFullscreen",
+			"webkitExitFullscreen",
+			"webkitExitFullScreen",
+			"webkitCancelFullScreen",
+			"mozCancelFullScreen",
+			"msExitFullscreen",
+		].find((m) => (document && m in document) || (targetRef && m in targetRef)) as
+			| "exitFullscreen"
+			| undefined;
+	});
+
+	const fullscreenEnabled = $derived.by<"fullscreenEnabled" | undefined>(() => {
+		return [
+			"fullScreen",
+			"webkitIsFullScreen",
+			"webkitDisplayingFullscreen",
+			"mozFullScreen",
+			"msFullscreenElement",
+		].find((m) => (document && m in document) || (targetRef && m in targetRef)) as
+			| "fullscreenEnabled"
+			| undefined;
+	});
+
+	const fullscreenElementMethod = [
+		"fullscreenElement",
+		"webkitFullscreenElement",
+		"mozFullScreenElement",
+		"msFullscreenElement",
+	].find((m) => document && m in document) as "fullscreenElement" | undefined;
+
+	const isSupported = $derived(
+		!!(targetRef && document && requestMethod && exitMethod && fullscreenEnabled)
+	);
+
+	const isCurrentElementFullScreen = (): boolean => {
+		if (fullscreenElementMethod) return document?.[fullscreenElementMethod] === targetRef;
+		return false;
+	};
+
+	const isElementFullScreen = (): boolean => {
+		if (fullscreenEnabled) {
+			if (document && document[fullscreenEnabled] != null) {
+				return document[fullscreenEnabled];
+			} else {
+				const target = targetRef;
+				// @ts-expect-error - Fallback for WebKit and iOS Safari browsers
+				if (target?.[fullscreenEnabled] != null) {
+					// @ts-expect-error - Fallback for WebKit and iOS Safari browsers
+					return Boolean(target[fullscreenEnabled]);
+				}
+			}
+		}
+		return false;
+	};
+
 	async function exit() {
-		await document.exitFullscreen();
+		if (!isSupported || !isFullscreen) return;
+		if (exitMethod) {
+			if (document?.[exitMethod] != null) {
+				await document[exitMethod]();
+			} else {
+				const target = targetRef;
+				// @ts-expect-error - Fallback for Safari iOS
+				if (target?.[exitMethod] != null)
+					// @ts-expect-error - Fallback for Safari iOS
+					await target[exitMethod]();
+			}
+		}
+
+		isFullscreen = false;
 	}
+
 	async function enter() {
-		await el()?.requestFullscreen();
-	}
-	async function toggle() {
-		if (!el()) return;
-		if (isFullscreen) {
-			await exit();
-		} else {
-			await enter();
+		if (!isSupported || isFullscreen) return;
+
+		if (isElementFullScreen()) await exit();
+
+		const target = targetRef;
+		if (requestMethod && target?.[requestMethod] != null) {
+			await target[requestMethod]();
+			isFullscreen = true;
 		}
 	}
-	function updateFullscreenState() {
-		isFullscreen = !!document.fullscreenElement;
+
+	async function toggle() {
+		await (isFullscreen ? exit() : enter());
 	}
+
+	const handlerCallback = () => {
+		const isElementFullScreenValue = isElementFullScreen();
+		if (!isElementFullScreenValue || (isElementFullScreenValue && isCurrentElementFullScreen()))
+			isFullscreen = isElementFullScreenValue;
+	};
+
+	const listenerOptions = { capture: false, passive: true };
+	useEventListener(document, eventHandlers, handlerCallback, listenerOptions);
+	useEventListener(() => targetRef, eventHandlers, handlerCallback, listenerOptions);
+
 	$effect(() => {
-		document.addEventListener("fullscreenchange", updateFullscreenState);
+		handlerCallback();
+
 		return () => {
-			document.removeEventListener("fullscreenchange", updateFullscreenState);
+			if (autoExit) {
+				exit();
+			}
 		};
 	});
+
 	return {
+		get isSupported() {
+			return isSupported;
+		},
 		get isFullscreen() {
 			return isFullscreen;
 		},
